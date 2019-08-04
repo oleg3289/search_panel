f04df345d509be347a6f0444c4d64380c67f62cf
Sytnik Oleg <olegsitnik2011@gmail.com>
2019-08-04 16:38:59 +0300

    protorol has been changed to https

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -9,6 +9,6 @@ export class UsersService {
     constructor(private http: HttpClient) {}
 
     getUser(userName: string): Observable<any> {
-        return this.http.get(`http://api.github.com/users/${userName}?client_id=b05c2048d78879b5e92a&client_secret=ea2e2d2f3be39ba74d4727894be2714ac1381b04`)
+        return this.http.get(`https://api.github.com/users/${userName}?client_id=b05c2048d78879b5e92a&client_secret=ea2e2d2f3be39ba74d4727894be2714ac1381b04`)
     }
 }
